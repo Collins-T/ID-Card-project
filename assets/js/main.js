dedf383577ccb7d171b52ff3b251908723ac0a50
Ghislain <melahghislain17@gmail.com>
2020-10-02 09:44:23 +0100

    made a change

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ const close = document.querySelector('.close');
 const open = document.querySelector('.navbar-toggler');
 const wrapper = document.querySelector('.wrapper');
 
+
+// sidebar functionality use to toggle sidebar menu
 if(outerWidth < 768){
     wrapper.classList.add('hidden');
 }
