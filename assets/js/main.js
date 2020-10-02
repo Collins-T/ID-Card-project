b001f86ee7142caced29b61ccd093c5b9f5fb88a
Ghislain <melahghislain17@gmail.com>
2020-10-02 07:58:23 +0100

    made a change

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ const close = document.querySelector('.close');
 const open = document.querySelector('.navbar-toggler');
 const wrapper = document.querySelector('.wrapper');
 
-/*if(outerWidth < 768){
+if(outerWidth < 768){
     wrapper.classList.add('hidden');
-}*/
+}
 close.addEventListener('click', () =>{
     wrapper.classList.toggle('hidden');
 })
